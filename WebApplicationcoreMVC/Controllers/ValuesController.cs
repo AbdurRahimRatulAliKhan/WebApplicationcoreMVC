0b726604af0c68d8a2e49e2519eed59d8d57dd8f
Ratul Ali <rahim498617@gmail.com>
2022-04-09 15:01:07 +0600

    Added GET, PUT, POST  and DELETE Verbs

--- WebApplicationcoreMVC/Controllers/ValuesController.cs
+++ WebApplicationcoreMVC/Controllers/ValuesController.cs
@@ -9,18 +9,38 @@ namespace WebApplicationcoreMVC.Controllers
     [Authorize]
     public class ValuesController : ControllerBase
     {
+        static List<string> strings = new List<string>()
+        {
+           "value0", "value1", "value2"
+        };
         //Get api/values
         public IEnumerable<string> Get()
         {
-            return new[] { "value1", "value2" };
+            return strings;
         }
 
         //GET values/5
         public string Get(int id)
         {
-            return "value";
+            return strings[id];
         }
 
-       
+        //POST api/values
+        public void Post([FromBody] string value)
+        {
+            strings.Add(value);
+        }
+
+        //PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+            strings[id] = value;
+        }
+
+        //DELETE api/values/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);
+        }
     }
 }
