f1ff9b0bf618268986f50e68f4fd861801f342cc
Ratul Ali <rahim498617@gmail.com>
2022-04-09 12:39:29 +0600

    Add project files.

---
+++ WebApplicationcoreMVC/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationcoreMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        //Get api/values
+        public IEnumerable<string> Get()
+        {
+            return new[] { "value1", "value2" };
+        }
+
+        //GET values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+       
+    }
+}
